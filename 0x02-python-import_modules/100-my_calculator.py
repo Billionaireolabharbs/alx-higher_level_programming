af13c187939168d556ceaec4e4b478599364b439
Babatunde <bharbsolaoluwak@gmail.com>
2022-12-01 12:03:44 -0800

    cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,23 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-     argc = len(sys.argv) - 1
-     argv = sys.argv
-      operators = {
-        '+': 'add(a, b)',
-        '-': 'sub(a, b)',
-        '*': 'mul(a, b)',
-        '/': 'div(a, b)'
-    }
-
-      if argc is not 3:
-          print("Usage: ./100-my_calculator.py <a> <operator> <b>:")
-          exit(1)
-      elif not argv[2] in operators:
-         print("Unknown operator. Available operators: +, -, * and /")
-          exit(1)
-      else:
-          a = int(argv[1])
-          b = int(argv[3])
-          print("{} {} {} = {}".format(a, argv[2], b, eval(operators[argv[2]])))
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    operators = ["+", "-", "*", "/"]
+    if argv_count is not 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] is "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] is "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] is "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif sys.argv[2] is "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
